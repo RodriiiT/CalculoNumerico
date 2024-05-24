d8b5999ab1e16ee5fb9fb003060c4311e399963e
Rodrigo <rodrileogabriel@gmail.com>
2024-05-23 23:56:00 -0400

    Se usa diseño de página de la documentación de Flet

--- Evaluaciones/Evaluación1/venv_py37/InterfazFlet/Menu/main.py
+++ Evaluaciones/Evaluación1/venv_py37/InterfazFlet/Menu/main.py
@@ -1,8 +1,40 @@
 import flet as ft
 
+def main(page: ft.Page) -> None:
+    page.title = "Evaluación 1"
 
-def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    def route_change(e: ft.RouteChangeEvent) -> None:
+        page.views.clear()
+        page.views.append(
+            ft.View(
+                '/',
+                [
+                    ft.AppBar(title=ft.Text("Inicio"), bgcolor='blue'),
+                    ft.Text(value='Menú', size=30,color='black'),
+                    ft.ElevatedButton("Visit Store", on_click=lambda _: page.go("/store")),
+                ],
+            )
+        )
+        if page.route == "/Store":
+            page.views.append(
+                ft.View(
+                    "/store",
+                    [
+                        ft.AppBar(title=ft.Text("Store"), bgcolor=ft.colors.SURFACE_VARIANT),
+                        ft.ElevatedButton("Go Home", on_click=lambda _: page.go("/")),
+                    ],
+                )
+            )
+        page.update()
+
+    def view_pop(view):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+    page.on_route_change = route_change
+    page.on_view_pop = view_pop
+    page.go(page.route)
 
 
-ft.app(main)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
