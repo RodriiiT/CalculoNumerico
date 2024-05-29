1743365c3a2f42c42e71b5fa88ac9e1fec7d86e0
Rodrigo <rodrileogabriel@gmail.com>
2024-05-29 15:02:11 -0400

    Se añade la práctica número 3 realizada en el lenguaje R

---
+++ Practicas/Practica3/operadores.R
@@ -0,0 +1,24 @@
+#Suma
+6 + 10
+
+#Resta
+10 - 5
+
+#Multiplicación
+5*5
+
+#División
+25 / 5
+
+#Módulo
+17%%3
+
+#Potencia
+2^2
+
+#Orden de operaciones
+2*(3+2)
+#Resultado = 10
+
+2*3+2
+#Resultado = 8
---
+++ Practicas/Practica3/operadoresLogicos.R
@@ -0,0 +1,25 @@
+#Operadores de comparación
+
+#Igualdad
+
+5 == 3
+
+#Desigualdad
+
+5 != 3
+
+#Menor que
+
+5 < 3
+
+#Mayor que
+
+5 > 3
+
+#Menor o igual que
+
+5 <= 3
+
+#Mayor o igual que
+
+5 >= 3
